20d4567b0f2c5b17a0afa52e45eb0c81f6b32158
MrTold11 <mrtold11@gmail.com>
2021-08-10 16:40:16 +0300

    Fixes

--- popaguy.py
+++ popaguy.py
@@ -5,18 +5,21 @@ import threading
 import base64
 import requests
 
+REC_LIMIT = 15
+MIN_VOLUME = 250
 BUFFER_VOICE = 10
 T_MIN = 10
 T_MAX = 420 #Divide this by ~4
 MUL_CONST = 1.570796326794896 / (T_MAX - T_MIN)
 AZURE_TOKEN = "token"
-IP = "http://192.168.234.16:8080/process"
+IP = "http://192.168.234.76:8080/process"
 #IP = "http://192.168.234.39:4455/"
 VOICE_PATH = "voice/aud"
 VOICE_FORM = ".wav"
 
 r = sr.Recognizer()
-r.energy_threshold = 200
+r.energy_threshold = MIN_VOLUME
+r.dynamic_energy_threshold = False
 counter = 0
 freeSeconds = 0
 lastSpeak = time.time()
@@ -26,6 +29,9 @@ random.seed(lastSpeak)
 print("Init OK")
 
 def sendAudio(audio, id):
+    if len(audio.frame_data) < 200000:
+        print("Short - skip")
+        return
     with open("out/rec" + str(id) + ".wav", "wb") as f:
         f.write(audio.get_wav_data())
     req = requests.post(IP, files={"rec" + str(id) + ".d64" : (None, base64.b64encode(audio.get_wav_data()))})
@@ -33,15 +39,15 @@ def sendAudio(audio, id):
     if req.status_code == 200:
         print("Send OK")
         ed = str(req.content)[2:-1]
-        if len(ed) < 20:
+        if len(ed) < 40:
             print("Nothing")
         else:
             nid = getOldVoice()
             with open(VOICE_PATH + str(nid) + VOICE_FORM, "wb") as f:
                 f.write(base64.b64decode(ed))
             print("Phrase recieved")
-            call(["aplay", VOICE_PATH + str(nid) + VOICE_FORM, "-D", "hw:0,0"])
-            print("Playing...")
+#            call(["aplay", VOICE_PATH + str(nid) + VOICE_FORM, "-D", "hw:0,0"])
+#            print("Playing...")
     else:
         print("Send FAIL")
 
@@ -61,7 +67,8 @@ def getOldVoice():
 def tryPlay():
     global freeSeconds, lastSpeak
     deltaSpeak = int(time.time() - lastSpeak)
-    if mute or freeSeconds < 5: return
+    print("Delta: " + str(deltaSpeak) + "; free: " + str(freeSeconds))
+    if mute or freeSeconds < 1: return
     if random.randrange(100) < math.sin((deltaSpeak - T_MIN) * MUL_CONST) * 100:
         print("Speak after " + str(freeSeconds) + " seconds silence; " + str(deltaSpeak) + " seconds after the last speech")
         freeSeconds = 0
@@ -82,9 +89,10 @@ def tryPlay():
 while True:
     try:
         with sr.Microphone() as mic:
-            audio = r.listen(mic, 2)
-    except WaitTimeoutError:
-        freeSeconds += 2
+            audio = r.listen(mic, 1, REC_LIMIT)
+        tryPlay()
+    except sr.WaitTimeoutError:
+        freeSeconds += 1
         tryPlay()
         continue
     freeSeconds = 0
