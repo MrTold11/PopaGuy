80ce72196c806db886848aa45a51673e77929b2c
konstantiin <88653390+konstantiin@users.noreply.github.com>
2021-08-11 13:07:40 +0300

    Add files via upload

--- rt.py
+++ rt.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 import azure.cognitiveservices.speech as speechsdk
 import base64
 import wave
+from model import predict
 from azure.cognitiveservices.speech import AudioDataStream, SpeechConfig, SpeechSynthesizer
 from azure.cognitiveservices.speech.audio import AudioOutputConfig
 speech_config = speechsdk.SpeechConfig(subscription="29103316047b4c59a3edbadb3c899c81", endpoint="https://northeurope.api.cognitive.microsoft.com/sts/v1.0/issuetoken")
@@ -16,6 +18,18 @@ with open("E:/AD/dict/rec.wav", 'wb') as f:
 
 
 
+numbers = {'1':'один',
+           "2": "два",
+           "3": "три",
+           '4': 'четыре',
+           "5": "пять",
+           "6": "шесть",
+           '7': 'семь',
+           "8": "восемь",
+           "9": "девять",
+           "10": "десять"}
+
+
 
 def from_file(file_name):
 
@@ -27,19 +41,25 @@ def from_file(file_name):
 
 def gen_voice(text, output):
     result = []
-    for i in range(len(text)-1):
+    for i in range(len(text)):
 
         audio_config = AudioOutputConfig(filename=output + "sentence" + str(i) + '.wav')
         synthesizer = SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
         synthesizer.speak_text_async(text[i])
 
-sentences = from_file("E:/AD/dict/rec.wav").replace('?', '.').replace('!', '.').split('.')
-#a =[]
-#for x in sentences:
-#    a += x.split('?')
-#for x in sentences:
-#    a += x.split('!')
-#sentences = a
+
+sentences = from_file("E:/AD/dict/rec112.wav").replace('?', '.').replace('!', '.').split('.')
+text = []
+for i in sentences:
+    if i == '': continue
+    buf = i.lower().replace('!', '').replace('.', '').replace('?','').replace(',', '')
+    for j in buf.split():
+        if j in numbers:
+            buf = buf.replace(j, numbers[j])
+    t = predict(buf)
+    if t < -5:
+        print(t)
+        text.append(i)
 with open("E:/AD/num.txt", 'w') as f:
-    f.write(str(len(sentences)-1))
-gen_voice(sentences, "E:/AD/result/")
+    f.write(str(len(text)))
+gen_voice(text, "E:/AD/result/")
