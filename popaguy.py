19843bcaf4f035bc90be91236694d3a83e834aab
mrtold11 <mrtold11@gmail.com>
2021-08-12 12:01:39 +0300

    Audio sending error handle

--- popaguy.py
+++ popaguy.py
@@ -11,7 +11,7 @@ IP_SEND = "process"                #Server path to send audio data
 IP_AD = "listad"                   #Server path to get ad list
 IP_GETAD = "getad"                 #Server path to get ad file
 mute = False                       #Mute parrot?
-adMode = False                      #Play ad only (good for dining room / other loud place)
+adMode = False                     #Play ad only (good for dining room / other loud place)
 AD_MODE_TO = 5                     #How often to play ads in ad mode (sec)
 AD_TIMEOUT = 80                    #How often to play ads (sec)
 AD_SYNC_TO = 10                    #How often to sync ads (sec)
@@ -20,7 +20,7 @@ PROCESS_NOISE = False              #Remove noice and increase volume?
 REC_LIMIT = 15                     #Maximum record duration
 MIN_VOLUME = 220                   #Volume floor for record to start
 BUFFER_VOICE = 10                  #How many phrases to store?
-T_MIN = 5                         #Minimum time to pass before playing phrase (sec)
+T_MIN = 5                          #Minimum time to pass before playing phrase (sec)
 T_MAX = 210                        #Maximum time to pass before playing phrase (sec) (Divide this by ~4!)
 AZURE_TOKEN = "token"              #Token for Azure (todo)
 ADS_PATH = "ads"                   #Local path to store ad files
@@ -46,32 +46,36 @@ print("Init OK")
 
 # Send recorded audio to server and get new phrase or nothing
 def sendAudio(audio, id):
-    if len(audio.frame_data) < 100000:
-        print("Short - skip")
-        return
-    #recf = ap.AudioProcessing(audio.get_wav_data())
-    #if PROCESS_NOISE:
-    #    recf.processNoise(2, 0.7)
-    #recf.save_to_file("out/rec" + str(id) + WAV)
-    #with open("out/rec" + str(id) + WAV, "rb") as rf:
-    req = requests.post(IP + IP_SEND, files={"rec" + str(id) + ".d64" : (None, base64.b64encode(audio.get_wav_data()))})
-    if req.status_code == 200:
-        print("Send OK")
-        ed = str(req.content)[2:-1]
-        if len(ed) < 40:
-            print("Nothing")
+    try:
+        if len(audio.frame_data) < 100000:
+            print("Short - skip")
+            return
+        #recf = ap.AudioProcessing(audio.get_wav_data())
+        #if PROCESS_NOISE:
+        #    recf.processNoise(2, 0.7)
+        #recf.save_to_file("out/rec" + str(id) + WAV)
+        #with open("out/rec" + str(id) + WAV, "rb") as rf:
+        req = requests.post(IP + IP_SEND, files={"rec" + str(id) + ".d64" : (None, base64.b64encode(audio.get_wav_data()))})
+        if req.status_code == 200:
+            print("Send OK")
+            ed = str(req.content)[2:-1]
+            if len(ed) < 40:
+                print("Nothing")
+            else:
+                nid = getOldVoice()
+                #inrec = ap.AudioProcessing(base64.b64decode(ed))
+                #inrec.set_audio_speed(1.2)
+                #inrec.save_to_file(VOICE_PATH + str(nid) + WAV)
+                with open(VOICE_PATH + str(nid) + WAV, "wb") as ff:
+                    ff.write(base64.b64decode(ed))
+                print("Phrase recieved")
+#                call(["aplay", VOICE_PATH + str(nid) + WAV, "-D", "hw:0,0"])
+#                print("Playing...")
         else:
-            nid = getOldVoice()
-            #inrec = ap.AudioProcessing(base64.b64decode(ed))
-            #inrec.set_audio_speed(1.2)
-            #inrec.save_to_file(VOICE_PATH + str(nid) + WAV)
-            with open(VOICE_PATH + str(nid) + WAV, "wb") as ff:
-                ff.write(base64.b64decode(ed))
-            print("Phrase recieved")
-#            call(["aplay", VOICE_PATH + str(nid) + WAV, "-D", "hw:0,0"])
-#            print("Playing...")
-    else:
-        print("Send FAIL with code " + str(req.status_code))
+            print("Send FAIL with code " + str(req.status_code))
+    except:
+        print("An error ocupped during audio sending")
+        
 
 # Return id of the oldest file stored in VOICE_PATH
 def getOldVoice():
